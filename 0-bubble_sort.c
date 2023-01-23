0905e5e7f8620d875dc4f4018400b0883b5b5745
mahlet <akotet2123@gmail.com>
2023-01-22 16:48:23 -0800

    bubble sort correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,19 +12,24 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int c;
+	int c, swap;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		swap = 0;
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				c = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = c;
+				swap = 1;
 				print_array(array, size);
 			}
 		}
+		if (swap == 0)
+			break;
+		printf("lopping\n");
 	}
 }
