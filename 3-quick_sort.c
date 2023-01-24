a2ce24718c2f1afd3c129805f60ab6c925022aad
mahlet <akotet2123@gmail.com>
2023-01-24 01:07:17 -0800

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,42 +11,50 @@
 
 void quick_sort(int *array, size_t size)
 {
-	quicksort_recursion (array, 0, size - 1);
+	quicksort_recursion (array, 0, size - 1, size);
 }
 
-void quicksort_recursion (int *array, int low, int high)
+void quicksort_recursion (int *array, int low, int high, size_t size)
 {
 	int pivot_index;
 
 	if (low < high)
 	{
-		pivot_index = partition (array, low, high);
+		pivot_index = partition (array, low, high, size);
 		/** left hand portion */
-		quicksort_recursion (array, low, pivot_index - 1);
+		quicksort_recursion (array, low, pivot_index - 1, size);
 		/** right hand portion */
-		quicksort_recursion (array, pivot_index + 1, high);
+		quicksort_recursion (array, pivot_index + 1, high, size);
 	}
 }
 
-int partition (int *array, int low, int high)
+int partition (int *array, int low, int high, size_t size)
 {
-	int pivot_value, i = low, j, temp;
+	int pivot_value, i = low - 1, j, temp;
 
 	pivot_value = array[high];
 	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot_value)
 		{
+			i++;
 			/** swap (array[i], array[j]); */
+			if (i == j)
+				continue;
 			temp = array[i];
 			array[i] = array[j];
 			array[j] = temp;
-			i++;
+			print_array(array, size);
 		}
 	}
 	/** swap (array[i], array[high]); */
-	temp = array[i];
-	array[i] = array[high];
-	array[high] = temp;
+	i++;
+	if (i != high)
+	{
+		temp = array[i];
+		array[i] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
 	return (i);
 }
