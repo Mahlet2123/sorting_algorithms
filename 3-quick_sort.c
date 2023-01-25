053733b17195b7020e4532c0c4fcf2ffdb5e8613
mahlet <akotet2123@gmail.com>
2023-01-24 18:21:12 -0800

    betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,28 +7,43 @@
  * @array: The array to be sorted
  * @size: Number of elements in @array
  * Return: void
- */ 
-
+ */
 void quick_sort(int *array, size_t size)
 {
-	quicksort_recursion (array, 0, size - 1, size);
+	quicksort_recursion(array, 0, size - 1, size);
 }
 
-void quicksort_recursion (int *array, int low, int high, size_t size)
+/**
+ * quicksort_recursion- recursive function
+ * @array: The array to be sorted
+ * @high: hign index (pivot value)
+ * @low: low index
+ * @size: Number of elements in @array
+ * Return: void
+ */
+void quicksort_recursion(int *array, int low, int high, size_t size)
 {
 	int pivot_index;
 
 	if (low < high)
 	{
-		pivot_index = partition (array, low, high, size);
+		pivot_index = partition(array, low, high, size);
 		/** left hand portion */
-		quicksort_recursion (array, low, pivot_index - 1, size);
+		quicksort_recursion(array, low, pivot_index - 1, size);
 		/** right hand portion */
-		quicksort_recursion (array, pivot_index + 1, high, size);
+		quicksort_recursion(array, pivot_index + 1, high, size);
 	}
 }
 
-int partition (int *array, int low, int high, size_t size)
+/**
+ * partition- divides the array around pivot value
+ * @array: The array to be sorted
+ * @high: hign index (pivot value)
+ * @low: low index
+ * @size: Number of elements in @array
+ * Return: int(pivot index)
+ */
+int partition(int *array, int low, int high, size_t size)
 {
 	int pivot_value, i = low - 1, j, temp;
 
