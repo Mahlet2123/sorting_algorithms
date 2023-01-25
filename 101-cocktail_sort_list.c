0f9fe449d28cdcdfd389a3e7fb52cc810b58920e
mahlet <akotet2123@gmail.com>
2023-01-25 15:02:35 -0800

    cocktail sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -31,37 +31,38 @@ void cocktail_sort_list(listint_t **list)
 	listint_t *temp;
 	int swaped;
 
-	if (list == NULL || (*list) == NULL)
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
 		return;
 	temp = (*list);
-	swaped = 1;
-	while (swaped)
+	do
 	{
+		swaped = 0;
 		while (temp->next)
 		{
 			if (temp->n > temp->next->n)
 			{
 				swap_list(temp, temp->next);
-				swaped = 0;
+				swaped = 1;
 				if (temp->prev->prev == NULL)
 					*list = temp->prev;
-				print_list(*list);
-			}	
+				print_list((*list));
+			}
+			else
+				temp = temp->next;
 		}
-		if ((swaped = 1))
-			break;
 		while (temp->prev)
 		{
 			if (temp->n < temp->prev->n)
 			{
 				swap_list(temp->prev, temp);
-				swaped = 0;
+				swaped = 1;
 				if (temp->prev == NULL)
 					*list = temp;
-				print_list(*list);
+				print_list((*list));
 			}
+			else
+				temp = temp->prev;
 		}
-		if ((swaped = 1))
-			break;
-	}
+		temp = temp->next;
+	}while (swaped);
 }
